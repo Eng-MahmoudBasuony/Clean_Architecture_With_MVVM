fa1fd89e9791a024c8d908696e5a344f81616e2f
basuony <mbasuony83@gmail.com>
2022-05-24 23:21:08 +0200

    app bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:clean_architecture_with_mvvm/presentation/color_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/font_manager.dart';
+import 'package:clean_architecture_with_mvvm/presentation/styles_manager.dart';
 import 'package:clean_architecture_with_mvvm/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +11,21 @@ ThemeData getApplicationTheme() {
       primaryColorLight: ColorManager.lightPrimary,
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
+      splashColor: ColorManager.lightPrimary,//ripple effect color
       //--------- card view theme -----//
       cardTheme: CardTheme(
           color: ColorManager.white,
           shadowColor: ColorManager.grey,
-          elevation: AppSize.s4));
+          elevation: AppSize.s4),
+      //--------- app bar theme --------//
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ColorManager.primary,
+        elevation: AppSize.s4,
+        shadowColor: ColorManager.lightPrimary,
+        titleTextStyle: getRegularStyle(fontSize: FontSize.s16,color: ColorManager.white,)
+
+      )
+
+  );
 }
