1d0503f73d419da3c9bfec54d1f49f80474a70b3
basuony <mbasuony83@gmail.com>
2022-05-26 10:51:39 +0200

    fixed issue (Cannot mix 2018 and 2021 terms in call to TextTheme() constructor) https://docs-flutter-io.firebaseapp.com/flutter/material/TextTheme-class.html

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -53,12 +53,12 @@ ThemeData getApplicationTheme() {
       textTheme: TextTheme(
           displayLarge:
               getLightStyle(color: ColorManager.white, fontSize: FontSize.s22),
-          headline1: getRegularStyle(
+          headlineLarge: getRegularStyle(
               color: ColorManager.darkGrey, fontSize: FontSize.s16),
-          subtitle1: getMediumStyle(
+          titleMedium: getMediumStyle(
               color: ColorManager.lightGrey, fontSize: FontSize.s14),
-          caption: getRegularStyle(color: ColorManager.grey1),
-          bodyText1: getRegularStyle(color: ColorManager.grey)),
+          bodyLarge: getRegularStyle(color: ColorManager.grey1),
+          bodySmall: getRegularStyle(color: ColorManager.grey)),
 
       ///---- input Decoration Theme (text from field) ----//
       inputDecorationTheme: InputDecorationTheme(
--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_with_mvvm/presentation/resources/assets_manager.dart';
 import 'package:clean_architecture_with_mvvm/presentation/resources/color_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -11,9 +12,13 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          backgroundColor: ColorManager.primary,
-          
+    return const Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: Center(
+        child: Image(
+          image: AssetImage(ImageAssets.splashLogo),
+        ),
+      ),
     );
   }
 }
